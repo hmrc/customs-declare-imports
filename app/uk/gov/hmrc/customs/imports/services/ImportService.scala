69747cc5f1562e560a51c200307f0429106d7e3b
matt.clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2019-01-29 16:00:59 +0000

    CDSID-575 : added more tests and tidy up

--- app/uk/gov/hmrc/customs/imports/services/ImportService.scala
+++ app/uk/gov/hmrc/customs/imports/services/ImportService.scala
@@ -1,13 +1,40 @@
 package uk.gov.hmrc.customs.imports.services
 
 import javax.inject.{Inject, Singleton}
+import play.api.Logger
+import play.api.libs.json.Json
 import uk.gov.hmrc.customs.imports.connectors.CustomsDeclarationsConnector
-import uk.gov.hmrc.customs.imports.models.{Eori, LocalReferenceNumber}
+import uk.gov.hmrc.customs.imports.controllers.ErrorResponse
+import uk.gov.hmrc.customs.imports.models.{ImportsResponse, Submission}
 import uk.gov.hmrc.customs.imports.repositories.SubmissionRepository
+import uk.gov.hmrc.http.HeaderCarrier
+import play.api.mvc.Results._
+import play.api.http.Status._
+import play.api.mvc.Result
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.xml.NodeSeq
 
 @Singleton
-class ImportService @Inject()(submissionRepository: SubmissionRepository, customsDeclarationsConnector: CustomsDeclarationsConnector) {
-  def handleDeclarationSubmit(eori: Eori, localReferenceNumber: LocalReferenceNumber) = ???
+class ImportService @Inject()(submissionRepository: SubmissionRepository, customsDeclarationsConnector: CustomsDeclarationsConnector)
+                             (implicit hc: HeaderCarrier) {
+  def handleDeclarationSubmit(eori: String, localReferenceNumber: String, xml: NodeSeq): Future[Result] = {
+    customsDeclarationsConnector.submitImportDeclaration(eori, xml.toString()).flatMap({ response =>
+      Logger.debug(s"conversationId: ${response.conversationId}")
+      submissionRepository
+        .save(Submission(eori, localReferenceNumber, None))
+        .map({ res =>
+          if (res.ok) {
+            Logger.debug("submission data saved to DB")
+            Ok(Json.toJson(ImportsResponse(OK, "Submission response saved")))
+          } else {
+            Logger.error("error saving submission data to DB")
+            ErrorResponse.ErrorInternalServerError.XmlResult
+          }
+        })
+    })
+  }
 
 
 }
--- test/unit/services/ImportServiceSpec.scala
+++ test/unit/services/ImportServiceSpec.scala
@@ -5,27 +5,42 @@ import org.mockito.ArgumentMatchers._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
 import reactivemongo.api.commands.WriteResult
-import uk.gov.hmrc.customs.imports.models.Submission
+import uk.gov.hmrc.customs.imports.connectors.{CustomsDeclarationsConnector, CustomsDeclarationsResponse}
+import uk.gov.hmrc.customs.imports.models.{Eori, LocalReferenceNumber, Submission}
 import uk.gov.hmrc.customs.imports.repositories.SubmissionRepository
 import uk.gov.hmrc.customs.imports.services.ImportService
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.test.UnitSpec
 import unit.base.ImportsTestData
+import play.api.mvc.Results._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class ImportServiceSpec extends MockitoSugar with UnitSpec with ScalaFutures with ImportsTestData{
 
   trait SetUp {
     val mockSubmissionRepo = mock[SubmissionRepository]
-    val testObj = new ImportService(mockSubmissionRepo)
+    val mockCustomsDeclarationsConnector = mock[CustomsDeclarationsConnector]
+    implicit val hc = mock[HeaderCarrier]
+    val testObj = new ImportService(mockSubmissionRepo, mockCustomsDeclarationsConnector)
   }
 
   "ImportService" should {
     "save submission Data in repository" in new SetUp() {
       val mockWriteResult = mock[WriteResult]
-      when(mockSubmissionRepo.save(any[Submission])).thenReturn(Future.successful(mockWriteResult))
 
-      testObj.handleDeclarationSubmit(submission)
+      when(mockCustomsDeclarationsConnector.submitImportDeclaration(any[String], any[String])(any[HeaderCarrier],any[ExecutionContext]))
+        .thenReturn(Future.successful(CustomsDeclarationsResponse(randomConversationId)))
+      when(mockSubmissionRepo.save(any[Submission])).thenReturn(Future.successful(mockWriteResult))
+      when(mockWriteResult.ok).thenReturn(true)
+
+      val xmlVal = <iamxml></iamxml>
+      val result = testObj.handleDeclarationSubmit(declarantEoriValue, declarantLrnValue,  xmlVal).futureValue
+
+      verify(mockCustomsDeclarationsConnector, times(1)).submitImportDeclaration(any[String], any[String])(any[HeaderCarrier],any[ExecutionContext])
+      verify(mockSubmissionRepo, times(1)).save(any[Submission])
+
+      result should be(Ok)
     }
   }
 }
