a6a6a0c8df7016ec7e6c7aebf7501f4081245f9a
Dave O'Brien <41006660+d6fb@users.noreply.github.com>
2019-02-13 11:12:16 +0000

    CDSID-793 - update dependencies (#30)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,15 +5,15 @@ import sbt._
 object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.9.0-play-25",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.12.0-play-25",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.7.0",
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.8.0",
     "uk.gov.hmrc" %% "wco-dec" % "0.18.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "4.1.0"
   )
 
   def test(scope: String = "test"): Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "3.1.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.4.0-play-25" % scope,
     "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % "test",
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
